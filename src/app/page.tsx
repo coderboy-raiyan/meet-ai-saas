f7c4f2b398ebd08c6d16b3222196c4d232a11dbf
Raiyan <tajkierhaque@gmail.com>
2025-06-26 10:54:49 +0600

    initial setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function home() {
+  return (
+    <div>
+      Hello world
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default home;
